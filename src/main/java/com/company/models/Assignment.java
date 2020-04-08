b16430e6d10a6c0a6cc58aacf7fd9d65b07d1403
Ozukyo <ozukyo@gmail.com>
2020-04-08 20:16:16 +0200

    Add assignment getters, setters, constructor

--- src/main/java/com/company/models/Assignment.java
+++ src/main/java/com/company/models/Assignment.java
@@ -1,4 +1,38 @@
 package com.company.models;
 
 public class Assignment {
+    private String title;
+    private int grade;
+    private boolean isSubmitted = false;
+    private String mentorName;
+
+    public Assignment(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setIsSubmittedTrue() {
+        isSubmitted = true;
+    }
+
+    public boolean checkIfAssignmentIsSubmitted() {
+        return isSubmitted;
+    }
+
+
 }
