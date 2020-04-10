cfbdb98af791cf01ae2cea15ae739a7b15fc2f57
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-04-10 05:57:30 +0200

    Add todo comments

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,8 +5,11 @@ import com.company.controllers.ApplicationController;
 import java.io.FileNotFoundException;
 
 // TODO:
-//      - attendance file appears after closing program
-//      -
+//      - Attendance file appears after closing program
+//      - Delete empty branches (2)
+//      - Handle more exceptions
+//      - While loops for inputs
+//      - Checks for nulls
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
