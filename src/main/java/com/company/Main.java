3190425b221f64d6d57a8f1b700e47417e50afef
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-04-10 12:52:58 +0200

    Branch push

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -10,6 +10,15 @@ import java.io.FileNotFoundException;
 //      - Handle more exceptions
 //      - While loops for inputs
 //      - Checks for nulls
+//                  getMentorFromListByUsername() {
+//                      return Optional.ofNullable(user)
+//                    }
+//      - more default accsess modifiers
+//      - in controllers constructors(User user, UserDao, dao) {}
+//      - more handled inputs and scanners
+//      - possibly make abstraction class CsvLine instead of List<List<String>>
+
+// comment just for push branch
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
